e212ea3e73f374a683949bad526a5738742a21f7
Marina Barcelo <marinadevops@gmail.com>
2024-04-19 15:24:15 +0200

    updates

---
+++ frontend/src/app/components/validations/validations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validations',
+  standalone: true,
+  imports: [],
+  templateUrl: './validations.component.html',
+  styleUrl: './validations.component.css'
+})
+export class ValidationsComponent {
+
+}
