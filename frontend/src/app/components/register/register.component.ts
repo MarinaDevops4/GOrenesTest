0c1a051630ba624ebfcb13cb1a68132c5d3475a9
Marina Barcelo <marinadevops@gmail.com>
2024-04-21 01:12:11 +0200

    update

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -13,8 +13,13 @@ export class RegisterComponent implements OnInit{
 registerForm: FormGroup;
 isloginUser:boolean = false;
 hiddenRegisterForm:boolean = false;
-inputType: string = 'password';
+inputTypePass: string = 'password';
+inputTypeConfirm: string = 'password';
 
+showErrorMessages:boolean = false;
+showSuccessMessage:boolean = false;
+
+success = '';
 
 
 
@@ -37,9 +42,13 @@ inputType: string = 'password';
     this.sharedService.setSharedVariable(false);
   }
 
-  togglePasswordVisibility() {
-    this.inputType = this.inputType === 'password' ? 'text' : 'password';
-  }
+  togglePasswordVisibility(controlName: string) {
+    if (controlName === 'password') {
+        this.inputTypePass = this.inputTypePass === 'password' ? 'text' : 'password';
+    } else if (controlName === 'confirmPassword') {
+        this.inputTypeConfirm = this.inputTypeConfirm === 'password' ? 'text' : 'password';
+    }
+}
 
   // registerUser(userData: any): void {
   //   this.userService.registerUser(userData)
@@ -57,10 +66,63 @@ inputType: string = 'password';
 
   onSubmit() {
     if(this.registerForm.valid){
-
+       // Verificar si las contraseñas coinciden
+    const password = this.registerForm.get('password')?.value;
+    const confirmPassword = this.registerForm.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      // Establecer el error de contraseña que no coincide en el formulario
+      this.registerForm.get('confirmPassword')?.setErrors({ 'passwordMismatch': true });
+      // Mostrar el mensaje de error
+      this.showErrorMessages = true;
+      return;
+    }
+      console.log('Form validated');
     }else{
+      this.showErrorMessages = true;
       console.log('Form not valid');
     }
+  }
+
+   // Determine if error should be shown for a control
+   shouldShowError(controlName: string) {
+    const control = this.registerForm.get(controlName);
+    return control?.invalid && (control?.dirty || control?.touched || this.showErrorMessages);
+  }
+  
+  // Get error message for a control
+  getErrorMessage(controlName: string) {
+    const control = this.registerForm.get(controlName);
+  
+    if (control?.hasError('required')) {
+      return 'Este campo es obligatorio';
+    } else if (controlName === 'email' && control?.hasError('email')) {
+      return 'El correo electrónico no es válido';
+    } else if (controlName === 'password' && control?.hasError('minlength')) {
+      return 'La contraseña debe tener al menos 6 caracteres';
+    } else if (controlName === 'confirmPassword' && control?.hasError('required')) {
+      return 'Debes confirmar la contraseña';
+    } else if (controlName === 'confirmPassword' && control?.hasError('passwordMismatch')) {
+      return 'Las contraseñas no coinciden';
+    } else if (controlName === 'accepted' && control?.hasError('requiredTrue')) {
+      return 'Debes aceptar los términos y condiciones';
+    }
+    return 'Error desconocido';
+  }
+
+  // Get success message
+  getSuccessMessage(): string {
+    return this.success;
+  }
+
+    // Mark control as invalid
+    markControlAsInvalid(controlName: string) {
+      const control = this.registerForm.get(controlName);
+      console.log(control);
+      if (control) {
+        control.markAsTouched(); 
+        control.markAsDirty(); 
+        control.setErrors({ 'invalid': true }); // Marcar el control como inválido
+      }
   }
 
 }
