125ad5147a3bfdd07d11a0610285347b20bbc99b
Marina Barcelo <marinadevops@gmail.com>
2024-04-22 18:20:24 +0200

    update

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,9 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../../services/Auth/authentication.service';
 import { Opcion } from './../../models/opcion/opcion.module';
 import { Pregunta } from './../../models/pregunta/pregunta.module';
 
 import { CommonModule } from '@angular/common';
+import { AuthenticationService } from '../../services/Auth/authentication.service';
 import jsonData from './../../../assets/storageData/info.json';
 @Component({
   selector: 'app-home',
@@ -17,84 +16,106 @@ import jsonData from './../../../assets/storageData/info.json';
 export class HomeComponent implements OnInit { 
   userName: string = '';
   userToken: any;
-  jsonData: any = jsonData; // Usar 'any' para el tipo de dato
+  jsonData: any = jsonData;
   preguntas: Pregunta[] = [];
   preguntaActual: Pregunta | null = null;
   respuestaSeleccionada: Opcion | null = null;
   intentosRestantes: number = 3;
-  puntaje: number = 0;
-  mostrarMensaje: boolean = false;
-  mensaje: string = '';
-
-  maxIntentos: number = 3; // Definir el máximo de intentos
-  preguntasPorJuego: number = 5;
-  fallos: number = 0; // Contador de fallos
-  aciertos: number = 0; // Contador de aciertos
+  aciertos: number = 0;
+  fallos: number = 0;
   juegoTerminado: boolean = false;
+  mostrarQuiz: boolean = false;
+  preguntasMostradas: Pregunta[] = [];
 
-  // Constructor del componente, inyecta el servicio de autenticación
-  constructor(private authService: AuthenticationService, private http:HttpClient) {}
+  constructor(private authService: AuthenticationService) {}
 
+  
   // Método que se ejecuta cuando el componente se inicializa
   ngOnInit(): void {
-   
     this.userToken = this.authService.getAccessToken(); 
-    // Llamada al servicio para obtener los detalles del usuario utilizando el token
     this.authService.getUserByToken(this.userToken).subscribe(
-        (userData) => {
-            this.userName = userData.username; 
-       
-        },
-        // Manejo de errores en la respuesta del servicio
-        (error) => {
-            console.error('Error al obtener los detalles del usuario:', error);
-        }
+      (userData) => {
+          this.userName = userData.username; 
+      },
+      (error) => {
+          console.error('Error al obtener los detalles del usuario:', error);
+      }
     );
-
+    // Cargar preguntas al iniciar el componente
     this.cargarPreguntas();
   }
 
-//    QUIZ
-cargarPreguntas() {
-  this.preguntas = this.jsonData.preguntas; // Asignar las preguntas del JSON directamente
-  this.mostrarPreguntaAleatoria();
-}
-
-mostrarPreguntaAleatoria() {
-  const randomIndex = Math.floor(Math.random() * this.preguntas.length);
-  this.preguntaActual = this.preguntas[randomIndex];
-  this.respuestaSeleccionada = null;
-}
-
-seleccionarRespuesta(respuesta: Opcion) {
-  // Lógica para evaluar la respuesta seleccionada y actualizar el estado del juego
-  if (respuesta.correcta) {
-    this.aciertos++;
-  } else {
-    this.fallos++;
+  // Función para cargar las preguntas y mostrar el quiz
+  cargarPreguntasYMostrarQuiz() {
+    this.cargarPreguntas();
+    this.mostrarQuiz = true;
   }
 
-  // Verificar si se alcanzaron los límites de fallos o aciertos
-  if (this.aciertos === this.preguntasPorJuego) {
-    this.mostrarMensaje = true;
-    this.mensaje = '¡Enhorabuena! Has acertado todas las preguntas.';
-    this.juegoTerminado = true;
-  } else if (this.fallos === this.maxIntentos) {
-    this.mostrarMensaje = true;
-    this.mensaje = 'Lo siento, has agotado tus intentos. Repasa la documentación de Angular.';
-    this.juegoTerminado = true;
-  } else {
+  // Cargar preguntas desde el JSON
+  cargarPreguntas() {
+    this.preguntas = this.jsonData.preguntas;
     this.mostrarPreguntaAleatoria();
   }
-}
-//    FIN QUIZ
-
-reiniciarJuego() {
-  this.puntaje = 0;
-  this.intentosRestantes = 3;
-  this.mostrarMensaje = false;
-  this.mostrarPreguntaAleatoria();
-}
 
 
+  // Mostrar una pregunta aleatoria
+  mostrarPreguntaAleatoria() {
+    // Filtrar las preguntas que aún no han sido mostradas
+    const preguntasDisponibles = this.preguntas.filter(pregunta => !this.preguntasMostradas.includes(pregunta));
+  
+    // Verificar si quedan preguntas disponibles para mostrar
+    if (preguntasDisponibles.length === 0) {
+      // Si ya se mostraron todas las preguntas, reiniciar el conjunto de preguntas mostradas
+      this.preguntasMostradas = [];
+      // Y volver a obtener las preguntas disponibles
+      this.mostrarPreguntaAleatoria();
+      return;
+    }
+  
+    // Seleccionar aleatoriamente una pregunta de las disponibles
+    const randomIndex = Math.floor(Math.random() * preguntasDisponibles.length);
+    const preguntaSeleccionada = preguntasDisponibles[randomIndex];
+  
+    // Actualizar la pregunta actual y agregarla al conjunto de preguntas mostradas
+    this.preguntaActual = preguntaSeleccionada;
+    this.respuestaSeleccionada = null;
+    this.preguntasMostradas.push(preguntaSeleccionada);
+  }
+  
+  // Selección de respuesta por el usuario
+  seleccionarRespuesta(respuesta: Opcion) {
+    if (respuesta.correcta) {
+      this.aciertos++;
+    } else {
+      this.fallos++;
+      this.intentosRestantes--;
+    }
+    this.actualizarEstadoJuego();
+  }
+
+
+   // Actualizar estado del juego
+   actualizarEstadoJuego() {
+    if (this.aciertos === 5 || this.fallos === 3) {
+      this.juegoTerminado = true;
+    } else {
+      this.mostrarPreguntaAleatoria();
+    }
+  }
+  // Mostrar mensaje al usuario
+  mostrarMensaje(mensaje: string) {
+    this.juegoTerminado = true;
+    this.preguntaActual = null;
+    this.respuestaSeleccionada = null;
+    alert(mensaje); // Mostrar mensaje mediante alerta, puedes personalizar esto según tus preferencias de UI
+  }
+
+   // Reiniciar el juego
+   reiniciarJuego() {
+    this.aciertos = 0;
+    this.fallos = 0;
+    this.intentosRestantes = 3;
+    this.juegoTerminado = false;
+    this.mostrarQuiz = false; // Ocultar el quiz al reiniciar el juego
+  }
 }
